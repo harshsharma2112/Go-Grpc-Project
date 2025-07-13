8912866ac638af0cfb913d674a9b75b742a4d265
Harsh kumar <harshkumar.h918@gmail.com>
2025-07-13 13:20:41 +0530

    Added server code

--- server/main.go
+++ server/main.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb.
+)
+
+const (
+	port = ":8080"
+)
+
+
+type helloserver struct{
+	pb.GreetServiceServer
+}
+
+func main() {
+	//using package for listen
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to start the server %v", err)
+	} //now i have the port
+
+	//now i will create the grpc server
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloserver{})
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to start the server %v", err)
+	}
+
+}
